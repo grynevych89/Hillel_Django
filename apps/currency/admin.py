5f674a82186a9b40359d5c93fe3aa593cf30d63b
grynevych89 <grynevych89@gmail.com>
2023-03-06 14:32:45 +0200

    dz 10

--- apps/currency/admin.py
+++ apps/currency/admin.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
+from rangefilter.filters import DateRangeFilter
 from currency.models import Rate
 
 
 @admin.register(Rate)
 class RateAdmin(admin.ModelAdmin):
-    list_display_links = ('id', 'currency')
+    list_display_links = ('id',)
     list_display = ['id', 'currency', 'sell', 'buy', 'source', 'created']
     list_editable = ('sell', 'buy', 'source')
-    list_filter = ['source']
-    fieldsets = (
-        ('Основне', {'fields': ('currency', 'sell', 'buy', 'source',), }),
+    list_filter = (
+        'currency',
+        ('created', DateRangeFilter)
+    )
+    search_fields = (
+        'source',
+        'buy',
+        'sell',
     )
