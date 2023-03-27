9128ab0fa18e30b9cd37b6cf95019787dbdbf432
grynevych89 <grynevych89@gmail.com>
2023-03-28 00:23:11 +0300

    done dz15

--- apps/source/models.py
+++ apps/source/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.templatetags.static import static
+
+
+def logo_path(instance, filename):
+    return f"logos/{instance.name}/{filename}"
 
 
 class Source(models.Model):
@@ -6,6 +11,17 @@ class Source(models.Model):
     text = models.TextField('Описание')
     source_url = models.URLField('URL', max_length=255)
     price = models.PositiveIntegerField('Ціна', default=0)
+    logo = models.FileField(
+        default=None,
+        null=True,
+        blank=True,
+        upload_to=logo_path)
+
+    @property
+    def logo_url(self):
+        if self.logo:
+            return self.logo.url
+        return static('default-logo.png')
 
     def __str__(self):
         return self.name
